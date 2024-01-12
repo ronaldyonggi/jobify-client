8bbf883a033295f00ba3b3b781027bb1d1f374f8
Ronald Yonggi <ronaldyonggi@gmail.com>
2024-01-12 16:34:51 +0800

    Designed Landing page

--- src/pages/Landing.tsx
+++ src/pages/Landing.tsx
@@ -1,6 +1,34 @@
+import Wrapper from '../assets/wrappers/LandingPage'
+import main from '../assets/images/main.svg'
+import logo from '../assets/images/logo.svg'
+import { Link } from 'react-router-dom'
+
 const Landing = () => {
   return (
-    <div>Landing</div>
+    <Wrapper>
+      <nav>
+        <img src={logo} alt="jobify" className='logo'/>
+      </nav>
+      <div className="container page">
+        <div className="info">
+          <h1>
+            job <span>tracking</span> app
+          </h1>
+          <p>
+            Lorem ipsum, dolor sit amet consectetur adipisicing elit. Neque tempora, aliquam illum pariatur, consequatur recusandae omnis aspernatur laborum expedita ab reprehenderit? Sit facere rerum recusandae quis, velit laborum sint. Odio!
+          </p>
+          <Link to="/register" className='btn register-link'>
+            Register
+          </Link>
+          <Link to="/register" className='btn'>
+            Login / Demo User
+          </Link>
+        </div>
+        <img src={main} alt="job hunt" className='img main-img' />
+      </div>
+    </Wrapper>
   )
 }
+
+
 export default Landing
