8147c244aa7e975b3776aa6cb2dbf91f5b2a6fa7
Ronald Yonggi <ronaldyonggi@gmail.com>
2024-01-13 15:17:03 +0800

    redesign Register page

--- src/pages/Register.tsx
+++ src/pages/Register.tsx
@@ -1,11 +1,20 @@
-import { Link } from "react-router-dom"
+import Wrapper from "../assets/wrappers/RegisterAndLoginPage"
+import { FormRow, Logo } from "../components"
 
 const Register = () => {
   return (
-    <div>
-      <h1>Register Page</h1>
-      <Link to='/login'>Login Page</Link>
-    </div>
+    <Wrapper>
+      <form className="form">
+        <Logo />
+        <h4>Register</h4>
+        <FormRow type="text" name="firstName" placeHolder="Enter first name" labelText="First Name" />
+        <FormRow type="text" name="lastName" placeHolder="Enter last name" labelText="Last Name" />
+        <FormRow type="text" name="location" placeHolder="Enter location" labelText="Location" />
+        <FormRow type="email" name="email" placeHolder="Enter email address" labelText="Email" />
+        <FormRow type="password" name="password" placeHolder="Enter password" labelText="Password" />
+        <button type="submit" className="btn btn-block">submit</button>
+      </form>
+    </Wrapper>
   )
 }
 export default Register
